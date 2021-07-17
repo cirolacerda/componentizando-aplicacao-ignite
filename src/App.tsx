7654a6e9773fe8abe6dbd4e2908a297e8e5a0ffd
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2021-07-17 16:44:53 -0300

    Refatorando, criando componente Content

--- src/App.tsx
+++ src/App.tsx
@@ -63,20 +63,8 @@ export function App() {
       
       <SideBar genres={genres} selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
      
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content movies={movies} genreTitle={selectedGenre.title}/>
+      
     </div>
   )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+  genreTitle: string;
+
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.genreTitle}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
